2b0e513225bb5a72ff2fa021cf5076afd4293f90
Doxam1 <143098976+doxam1@users.noreply.github.com>
2024-02-21 09:26:31 +0200

    basic grid allignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-document.write('this is a test')
+import './style.css';
+import Todo from './todoObj';
+
+const todoTest = new Todo ('test', 'go bom bom', new Date(2024, 3, 19, 13,30), 5);
+
+console.log(todoTest)
